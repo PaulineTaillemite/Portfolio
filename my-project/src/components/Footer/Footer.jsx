bd534c442538e999f1bad44f7c09b95863fd6055
Pauline Taillemite <paulinetaillemite@air-de-pauline.home>
2024-07-29 10:53:58 +0200

    set up the footer section

--- my-project/src/components/Footer/Footer.jsx
+++ my-project/src/components/Footer/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Logo from '.././assets/logo_gurl.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-darkcolor py-14'>
+
+      <div className="container flex flex-col gap-3 justify-center items-center">
+        <img src={Logo} alt="" className='w-[150px]' />
+        <p className='text-white text-center text-sm'> Copyright &copy; 2024. All Rights Reserved.</p>
+
+      </div>
+      
+    </div>
   )
 }
 
